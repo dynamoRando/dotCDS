d1708f364358075d875cae1e9fd4df4447b4cfcd
Randy Le <randyL04@hey.com>
2022-02-13 08:34:43 -0700

    rename

--- src/DotCDS/DotCDS/DatabaseClient/PostgresClient.cs
+++ src/DotCDS/DotCDS/DatabaseClient/PostgresClient.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DotCDS.Database
 {
-    internal class SQLServer
+    internal class PostgresClient
     {
     }
 }
--- src/DotCDS/DotCDS/DatabaseClient/SqliteClient.cs
+++ src/DotCDS/DotCDS/DatabaseClient/SqliteClient.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DotCDS.Database
 {
-    internal class Postgres
+    internal class SqliteClient
     {
     }
 }
