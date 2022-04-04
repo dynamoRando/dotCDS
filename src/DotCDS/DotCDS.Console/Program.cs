b81414743fc4481709daca4cdc0462f44b89611a
Randy Le <randyL04@hey.com>
2022-04-03 17:54:57 -0700

    Add notes for console

--- src/DotCDS/DotCDS.Console/Program.cs
+++ src/DotCDS/DotCDS.Console/Program.cs
@@ -6,11 +6,24 @@ namespace DotCDS.Console // Note: actual namespace depends on the project name.
     internal class Program
     {
         private static Process _process;
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("Welcome to dotCDS!");
+            System.Console.WriteLine("Press enter to start.");
+            var input = System.Console.ReadLine();
+            System.Console.WriteLine(("Starting dotCDS..."));
+
             _process = new Process();
             _process.Start();
+
+            while (!string.Equals(input, "q", System.StringComparison.OrdinalIgnoreCase))
+            {
+                System.Console.WriteLine("Waiting for input. Press 'q' to quit");
+                input = System.Console.ReadLine();
+            }
+
+            System.Console.WriteLine("Quitting dotCDS...");
         }
     }
 }
