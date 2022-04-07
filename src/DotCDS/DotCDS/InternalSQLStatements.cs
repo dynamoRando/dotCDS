b1969db7e7b3351ade86f03f8022b2e9cf99d650
Randy Le <randyL04@hey.com>
2022-04-06 21:14:07 -0700

    add docs

--- src/DotCDS/DotCDS/InternalSQLStatements.cs
+++ src/DotCDS/DotCDS/InternalSQLStatements.cs
@@ -44,8 +44,22 @@ namespace DotCDS
             // anything in COOP are tables stored in the user database used to enable cooperative functions with participants
             internal static class COOP
             {
+                // a list of participants for this user database
                 internal const string PARTICIPANT = "COOP_PARTICIPANT";
+
+                // a list of all the contracts that we have generated for this database
                 internal const string DATABASE_CONTRACT = "COOP_DATABASE_CONTRACT";
+
+                // the naming convention prefix for any table that is holding remote row references
+                // e.g. if the table is named 'EMPLOYEE_ADDRESS' and is remotable, the actual table holding the participants references would be named
+                // 'COOP_SHADOWS_EMPLOYEE_ADDRESS' and would just contain all the participants that would needed to be queried
+                // and the 'EMPLOYEE_ADDRESS' would just be an empty table
+                internal const string SHADOWS = "COOP_SHADOWS";
+
+                // contains a list of all the tables that have remote data turned on for them
+                // this tells CDS that rather than actually sending the request to Sqlite to query that table, 
+                // to instead look at the corresponding shadow table and request data from the participant(s)
+                internal const string REMOTES = "COOP_REMOTES";
             }
         }
 
