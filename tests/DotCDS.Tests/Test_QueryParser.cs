8201b1ff45516e086187eefc61e143e432f52f21
Randy Le <randyL04@hey.com>
2022-04-17 20:33:02 -0700

    make internals available to testing; scaffold testing query parser

---
+++ tests/DotCDS.Tests/Test_QueryParser.cs
@@ -0,0 +1,20 @@
+﻿using DotCDS.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotCDS.Tests
+{
+    public class Test_QueryParser
+    {
+        [Fact(Skip = "Test not written yet")]
+        public void Test_HasCooperative_Objects()
+        {
+            var parser = new QueryParser();
+
+        }
+    }
+}
