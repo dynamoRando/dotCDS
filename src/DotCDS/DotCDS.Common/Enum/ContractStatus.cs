f4df101383991791f1b7b114cd934a1d9daf634b
Randy Le <randyL04@hey.com>
2022-05-08 17:25:02 -0700

    testing contracts

---
+++ src/DotCDS/DotCDS.Common/Enum/ContractStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCDS.Common.Enum
+{
+    public enum ContractStatus
+    {
+        Unknown,
+        NotSent,
+        Pending,
+        Accepted,
+        Rejected
+    }
+}
